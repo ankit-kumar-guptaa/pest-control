0214d30119a1f99471fcc0093a6891a730e4ab66
ankit-kumar-guptaa <ankitkumarsahuu@gmail.com>
2023-10-05 15:29:38 +0530

    update

---
+++ include/footer.php
@@ -0,0 +1,145 @@
+<!-- Footer Start -->
+<div class="container-fluid footer py-5 wow fadeIn" data-wow-delay=".3s">
+      <div class="container py-5">
+        <div class="row g-4 footer-inner">
+          <div class="col-lg-3 col-md-6">
+            <div class="footer-item">
+              <h4 class="text-white fw-bold mb-4">About Modern Pest Control.</h4>
+              <p>
+              Modern Pest Control Services offer an absolute range of pest control services professionally at very reasonable cost. We are expert pest control service provider in Delhi NCR handle all types of pest problems in your residential and workplace areas.
+              </p>
+              <p class="mb-0">
+                <a class="" href="#">Modern PestControl </a> &copy; 2023 All Right
+                Reserved.
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6">
+            <div class="footer-item">
+              <h4 class="text-white fw-bold mb-4">Usefull Link</h4>
+              <div class="d-flex flex-column align-items-start">
+                <a class="btn btn-link ps-0" href=""
+                  ><i class="fa fa-check me-2"></i>About Us</a
+                >
+                <a class="btn btn-link ps-0" href=""
+                  ><i class="fa fa-check me-2"></i>Contact Us</a
+                >
+                <a class="btn btn-link ps-0" href=""
+                  ><i class="fa fa-check me-2"></i>Our Services</a
+                >
+                <a class="btn btn-link ps-0" href=""
+                  ><i class="fa fa-check me-2"></i>Terms & Condition</a
+                >
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6">
+            <div class="footer-item">
+              <h4 class="text-white fw-bold mb-4">Services Link</h4>
+              <div class="d-flex flex-column align-items-start">
+                <a class="btn btn-link ps-0" href=""
+                  ><i class="fa fa-check me-2"></i>Termite Control</a
+                >
+                <a class="btn btn-link ps-0" href=""
+                  ><i class="fa fa-check me-2"></i>Residental Pest Control</a
+                >
+                <a class="btn btn-link ps-0" href=""
+                  ><i class="fa fa-check me-2"></i>Comercial Pest Control</a
+                >
+                <a class="btn btn-link ps-0" href=""
+                  ><i class="fa fa-check me-2"></i>Cockroaches Control</a
+                >
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6" >
+            <div class="footer-item">
+              <h4 class="text-white fw-bold mb-4">Contact Us</h4>
+              <a
+                href=""
+                class="btn btn-link w-100 text-start ps-0 pb-3 border-bottom rounded-0"
+                ><i class="fa fa-map-marker-alt me-3"></i> <b> Delhi:</b> UB2C, I. P. Building, 21, B.K. Road, Connaught Place New Delhi-1</a
+              >
+
+              <a
+                href=""
+                class="btn btn-link w-100 text-start ps-0 pb-3 border-bottom rounded-0"
+                ><i class="fa fa-map-marker-alt me-3"></i><b> Ghaziabad :</b> 608-A, Nayaykhand-II, Indrapuram, Ghaziabad, U. P.</a
+              >
+
+              <a
+                href=""
+                class="btn btn-link w-100 text-start ps-0 py-3 border-bottom rounded-0"
+                ><i class="fa fa-phone-alt me-3"></i>+91-9899631591, +91-7827359696</a
+              >
+              <a
+                href="" style="text-transform: lowercase;"
+                class="btn btn-link w-100 text-start ps-0 py-3 border-bottom rounded-0"
+                ><i class="fa fa-envelope me-3"></i>devendrasharma.pestcontrol@gmail.com</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Footer End -->
+
+    <!-- Copyright Start -->
+    <div class="container-fluid copyright bg-dark py-4">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 text-center text-md-start mb-3 mb-md-0">
+            <a href="#" class="text-primary mb-0 display-6"
+              >Modern Pest <span class="text-white">Control</span
+              ><i class="fa fa-spider text-primary ms-2"></i
+            ></a>
+          </div>
+          <div
+            class="col-md-4 copyright-btn text-center text-md-start mb-3 mb-md-0 flex-shrink-0"
+          >
+            <a
+              class="btn btn-primary rounded-circle me-3 copyright-icon"
+              href=""
+              ><i class="fab fa-twitter"></i
+            ></a>
+            <a
+              class="btn btn-primary rounded-circle me-3 copyright-icon"
+              href=""
+              ><i class="fab fa-facebook-f"></i
+            ></a>
+            <a
+              class="btn btn-primary rounded-circle me-3 copyright-icon"
+              href=""
+              ><i class="fab fa-youtube"></i
+            ></a>
+            <a
+              class="btn btn-primary rounded-circle me-3 copyright-icon"
+              href=""
+              ><i class="fab fa-linkedin-in"></i
+            ></a>
+          </div>
+          <div class="col-md-4 my-auto text-center text-md-end text-white">
+            Designed By
+            <a class="border-bottom" href="https://adjointinfocom.com">Adjoint Infocom Private Limited</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Copyright End -->
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-primary rounded-circle border-3 back-to-top"
+      ><i class="fa fa-arrow-up"></i
+    ></a>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
